f306fb370b8c9267287eef1f5805b3a5f693dfd8
DuskWarden <pawar96sameer@gmail.com>
2025-11-04 12:57:20 +0530

    update main routes to include collaborator and invite routes

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const boardRoutes = require("./board-routes");
 const authRoutes = require("./auth-routes");
+const collaboratorRoutes = require("./collaborator-routes");
+const inviteRoutes = require("./invite-routes");
 
 const router = express.Router();
 
@@ -9,5 +11,11 @@ router.use("/auth", authRoutes);
 
 // Board routes
 router.use("/boards", boardRoutes);
+
+// Collaborator routes
+router.use("/collaborators", collaboratorRoutes);
+
+// Invite routes
+router.use("/invites", inviteRoutes);
 
 module.exports = router;
